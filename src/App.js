873b39301caa2b47c249ab07ab39ac23fa4c4005
peltonan <peltonen.annette@gmail.com>
2022-04-19 15:19:30 +0300

    Styling and Text

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
 
-    <div className="App">
 
+    <div className="App">
       <div className="headerGrid">
         <div>
         </div>
@@ -36,7 +36,18 @@ function App() {
       <section id="textAboutMe">
         <div className="container">
           <h2 className="textAboutMe">About Me</h2>
-          <p>Text about me</p>
+          <p>I'm a kindergarten teacher in the middle of a career change. I've lived in the Helsinki Metropolitan Area for 12 years studying and working.
+            I'm originally from the countryside where you can see farmlands and forests when looking outside the window.</p>
+
+          <p>I've worn many hats in the past and now I'm wearing one saying: Front End Developer. I've started in boot camp in January 2022, learning the basics of <span>HTML</span>, <span>CSS</span> and <span>Javascript</span>.
+            In March 2022, I started doing my own projects. I have two mentors who have been in the field for over 10 years, teaching and assisting me. Currently, I'm learning <span>React</span>.</p>
+
+          <p>I'm constantly learning new things and want to get better. I love working on the computer and have a passion for design. I also have skills in video editing, 3D modeling and art. I'm also interested in languages, I'm fluent in: English, Finnish and Swedish.</p>
+
+          <p>In my free time, I like to play video games, create art, spend time interior decorating, watching movies, reading books, walking outside and drinking tea. I also have a soft spot for music and theater.</p>
+
+          <p>In a nutshell: Gamer, artist, designer, introvert, nerd.</p>
+
         </div>
       </section>
 
